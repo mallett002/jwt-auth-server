1ab24a0fa5edabb93a2f4fb43fe4bc6f0f6c64be
Will Mallett <mallett002@gmail.com>
2021-02-02 06:51:58 -0600

    fix host and rename the app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [{
-    name: 'Tutorial',
+    name: 'jwt-auth-server',
     script: './index.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-14-37-193.us-east-2.compute.amazonaws.com/',
+      host: 'ec2-52-14-37-193.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/tutorial.pem',
       ref: 'origin/master',
       repo: 'git@github.com:mallett002/jwt-auth-server.git',
