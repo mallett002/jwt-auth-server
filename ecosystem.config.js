3fe587222f9c031978d07cbd1134de801e067d32
Will Mallett <mallett002@gmail.com>
2021-02-02 06:32:17 -0600

    adding ecosystem.config.js for pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Tutorial',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-14-37-193.us-east-2.compute.amazonaws.com/',
+      key: '~/.ssh/tutorial.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:mallett002/jwt-auth-server.git',
+      path: '/home/ubuntu/tutorial',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
