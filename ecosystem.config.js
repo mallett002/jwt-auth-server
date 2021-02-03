f7e69cd04c83e58ad24d0c86d5f55b45293eea2f
Will Mallett <mallett002@gmail.com>
2021-02-03 06:58:19 -0600

    update server host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-14-37-193.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-129-65-176.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/tutorial.pem',
       ref: 'origin/master',
       repo: 'git@github.com:mallett002/jwt-auth-server.git',
